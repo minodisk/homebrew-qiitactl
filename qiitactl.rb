e060b69af8cc12c2ff4d083e2295102bbcbda8ce
minodisk <daisuke.mino@gmail.com>
2016-02-19 11:54:31 +0900

    Update formula

--- qiitactl.rb
+++ qiitactl.rb
@@ -1,9 +1,9 @@
 class Qiitactl < Formula
   desc "Command line interface to manage the posts in Qitta."
   homepage "https://github.com/minodisk/qiitactl"
-  url "https://github.com/minodisk/qiitactl/releases/download/v0.1.1/qiitactl_0.1.1_darwin_amd64.zip"
-  version "v0.1.1"
-  sha256 "ad8b5972073f67e94b98caa73b6dcaa66e04a8681492a3049156eb05f369c74c"
+  url "https://github.com/minodisk/qiitactl/releases/download/v0.1.2/qiitactl_0.1.2_darwin_amd64.zip"
+  version "v0.1.2"
+  sha256 "8e0c8836b0e98efc0b4bf82eff4d806239514b30a8f019d367ae53e2fa281cab"
 
   depends_on :arch => :intel
 
